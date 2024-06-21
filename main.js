8ea72a97547875ad6dbb8c28c549b4dc14916bc8
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-06-21 18:37:36 +0530

    second commit

--- main.js
+++ main.js
@@ -1,23 +1,36 @@
-const fs = require('fs');
+const fs = require("fs");
 
+// Get the JSON file path from the command line arguments
 const jsonFilePath = process.argv[2];
 
-fs.readFile(jsonFilePath, 'utf8', (err, data) => {
+if (!jsonFilePath) {
+  console.error("Please provide a path to the JSON file.");
+  process.exit(1);
+}
+
+// Read the JSON file
+fs.readFile(jsonFilePath, "utf8", (err, data) => {
   if (err) {
     console.error(`Error reading file: ${err}`);
     return;
   }
 
-  const users = JSON.parse(data);
-
-  // TODO: Perform the required operations on the users data
+  let users;
+  try {
+    users = JSON.parse(data);
+  } catch (parseErr) {
+    console.error(`Error parsing JSON data: ${parseErr}`);
+    return;
+  }
 
   // Print the total number of users
   console.log(`Total number of users: ${users.length}`);
 
   // Find the user with the highest score and print their details
-  const highestScoreUser = users.reduce((prev, current) => (prev.score > current.score ? prev : current));
-  console.log('User with the highest score:', highestScoreUser);
+  const highestScoreUser = users.reduce((prev, current) =>
+    prev.score > current.score ? prev : current
+  );
+  console.log("User with the highest score:", highestScoreUser);
 
   // Sort the users based on their scores in descending order
   users.sort((a, b) => b.score - a.score);
@@ -28,6 +41,6 @@ fs.readFile(jsonFilePath, 'utf8', (err, data) => {
       console.error(`Error writing file: ${err}`);
       return;
     }
-    console.log('Data sorted and written back to the JSON file.');
+    console.log("Data sorted and written back to the JSON file.");
   });
 });
